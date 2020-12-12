2df851a56c1857da998a4a7f0fbe2c71b1aec1b3
Shrikantpaliwal <shrikantpaliwalgrt@gmail.com>
2020-12-13 01:58:29 +0530

    Transaction Layout Design

--- Personal_expense/lib/main.dart
+++ Personal_expense/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:Personal_expense/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,10 +17,10 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transaction = [
+  final List<Transaction> transactions = [
     Transaction(
         id: "t1", title: "New Shoes", amount: 100, date: DateTime.now()),
-    Transaction(id: "t2", title: "New GF", amount: 10000, date: DateTime.now())
+    Transaction(id: "t2", title: "New GF", amount: 150, date: DateTime.now())
   ];
 
   @override
@@ -39,10 +40,53 @@ class MyHomePage extends StatelessWidget {
               child: Text("Chart Will go here"),
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text("Transactions will go here"),
-            elevation: 5,
+          Column(
+            children: [
+              ...transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.green),
+                        ),
+                        child: Text(
+                          '\u20B9 ${tx.amount}',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green),
+                        ),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                          Text(
+                            DateFormat('yMEd').format(tx.date),
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList()
+            ],
           )
         ],
       ),
